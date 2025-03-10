dbeca9a1ab865aaf9ec5f35f0e58383f7c392a82
Miles <129598086+milestones14@users.noreply.github.com>
2025-03-10 14:22:57 +0100

    Create Settings.swift

---
+++ Macinate Menu Bar/Settings.swift
@@ -0,0 +1,55 @@
+//
+//  Settings.swift
+//  Macinate Menu Bar
+//
+//  Created by Meilo H. on 3/10/25.
+//
+
+import SwiftUI
+import ServiceManagement
+
+
+struct SettingsView: View {
+    @State private var isToggled = false
+    
+    init() {
+        let isRegistered = SMAppService.mainApp.status == .enabled
+        _isToggled = State(initialValue: isRegistered)
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+                .padding()
+            
+            Divider().frame(
+                        maxWidth: 100
+                    )
+            
+            Toggle("Open at login", isOn: $isToggled)
+                .onChange(of: isToggled) { newValue in
+                    handleToggleChange(to: newValue)
+                }
+                .padding()
+        }.frame(minWidth: 150, minHeight: 150)
+    }
+    
+    func handleToggleChange(to newValue: Bool) -> Void {
+        if newValue {
+           do {
+               try SMAppService.mainApp.register()
+               print("Successfully registered the app as a login item.")
+           } catch {
+               print("Failed to register as a login item: \(error)")
+           }
+       } else {
+           do {
+               try SMAppService.mainApp.unregister()
+               print("Successfully unregistered the app as a login item.")
+           } catch {
+               print("Failed to unregister as a login item: \(error)")
+           }
+       }
+    }
+}
